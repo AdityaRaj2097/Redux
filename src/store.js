34233543d3cdbea1fce984376510a201507821cb
AdityaRaj2097 <72922589+AdityaRaj2097@users.noreply.github.com>
2021-07-17 20:22:26 +0530

    fetect user by redux

--- src/store.js
+++ src/store.js
@@ -1,5 +1,11 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./redux/rootreducer";
+import thunk from "redux-thunk";
 console.log("Hello");
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+// import thunk from "redux-thunk";
+// // import rootReducer from "./redux/rootReducer";
+// console.log("Hello");
+// const store = createStore(rootReducer, applyMiddleware(thunk));
 export default store;
